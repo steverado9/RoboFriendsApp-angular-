20eae6df90bbaca27965f264f71345c9e613fced
stephen <isaacsteverado9@gmail.com>
2025-04-26 01:53:13 +0100

    angular

--- src/app/components/search-box/search-box.component.ts
+++ src/app/components/search-box/search-box.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { log } from 'console';
 import {FormsModule} from '@angular/forms';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-search-box',
-  imports: [FormsModule],
+  imports: [FormsModule, CardComponent],
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
