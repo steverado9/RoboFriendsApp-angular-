508bc762fa8281af70b8e644e56496aff3281042
stephen <isaacsteverado9@gmail.com>
2025-04-29 01:49:48 +0100

    trying to fetch the api

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
 
+// @Injectable({providedIn: 'root'})
+
+export class CardComponent {
+  // private http = inject(HttpClient);
 }
